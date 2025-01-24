8577786437368d29971012ee42dd21f4feb1628b
VasigAG <agvasig19@gmail.com>
2025-01-25 01:18:02 +0530

    fuckkk

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
